003fa63bd789e566350ee301917504326c7db837
developper <53014479+developper001@users.noreply.github.com>
2024-08-04 19:00:59 +0200

    Add a thread safe random number generator.

--- GrpcGreeterClient/Program.cs
+++ GrpcGreeterClient/Program.cs
@@ -5,8 +5,17 @@ using GrpcGreeterClient;
 // The port number must match the port of the gRPC server.
 using var channel = GrpcChannel.ForAddress("https://localhost:7213");
 var client = new Greeter.GreeterClient(channel);
-var reply = await client.SayHelloAsync(
-                  new HelloRequest { Name = "GreeterClient" });
-Console.WriteLine("Greeting: " + reply.Message);
-Console.WriteLine("Press any key to exit...");
-Console.ReadKey();
+var reply1 = await client.SayHelloAsync(
+                  new HelloRequest { Name = "GreeterClient1" });
+Console.WriteLine("Greeting: " + reply1.Message);
+
+
+// Say random number
+var reply2 = await client.SayRandomNumberAsync(new HelloRequest { Name = "GreeterClient2" });
+Console.WriteLine("Random message server response : " + reply2.Message);
+
+
+// End
+Console.WriteLine("Success.");
+// Console.WriteLine("Press any key to exit...");
+// Console.ReadKey();
--- GrpcGreeterServer/Services/GreeterService.cs
+++ GrpcGreeterServer/Services/GreeterService.cs
@@ -6,6 +6,23 @@ namespace grpcAot2024.Services;
 public class GreeterService : Greeter.GreeterBase
 {
     private readonly ILogger<GreeterService> _logger;
+    private static readonly Random _global = new Random();
+    [ThreadStatic] private static Random _local;
+
+    private int Next(int min, int max)
+    {
+        if (_local == null)
+        {
+            int seed;
+            lock (_global)
+            {
+                seed = _global.Next();
+            }
+            _local = new Random(seed);
+        }
+        return _local.Next(min, max);
+    }
+
     public GreeterService(ILogger<GreeterService> logger)
     {
         _logger = logger;
@@ -13,10 +30,21 @@ public class GreeterService : Greeter.GreeterBase
 
     public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
     {
-        // _logger.LogInformation("SayHello {0}", request.Name);
+        _logger.LogInformation("SayHello! {0}", request.Name);
         return Task.FromResult(new HelloReply
         {
             Message = "Hello " + request.Name
+        });
+    }
+
+    public override Task<HelloReply> SayRandomNumber(HelloRequest request, ServerCallContext context)
+    {
+        _logger.LogInformation("SayRandomNumber: {0}", request.Name);
+        var randomNumber = Next(1, 100);
+        var outputMessage = $"The random number is {randomNumber}";
+        return Task.FromResult(new HelloReply
+        {
+            Message = outputMessage
         });
     }
 }
